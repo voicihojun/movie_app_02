371b35b1478f77c3711cc1cd2cf74f3bed01c4b8
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2021-08-27 15:11:40 -0400

    change design : all functional thing in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,106 @@
 import SearchBar from "./SearchBar";
+import Nominations from "./Nominations";
+import ResultFor from "./ResultFor";
+import Banner from "./Banner";
+import axios from "axios";
+import React, { useState, useRef } from "react";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    text: "",
+    movies: [],
+    find: true,
+  });
+  const [nominees, setNominees] = useState([]);
+  const [banner, setBanner] = useState(false);
+  const { text, movies, find } = inputs;
+  const focusInput = useRef();
+
+  const onChange = (e) => {
+    setInputs({
+      ...inputs,
+      text: e.target.value,
+    });
+  };
+
+  const API_KEY = "https://www.omdbapi.com/?apikey=9cbee72";
+
+  const fetchMovies = async () => {
+    const {
+      data: { Search },
+    } = await axios.get(`${API_KEY}&&s=${text}`);
+    if (Search === undefined) {
+      setInputs({
+        ...inputs,
+        find: false,
+      });
+    } else {
+      setInputs({
+        ...inputs,
+        movies: Search,
+        find: true,
+      });
+    }
+    focusInput.current.focus();
+  };
+
+  const onNomination = async (e) => {
+    const imdbID = e.target.parentElement.firstChild.innerText;
+    const API = `${API_KEY}&i=${imdbID}`;
+    const info = await axios.get(API);
+
+    const id = info.data.imdbID;
+    const year = info.data.Year;
+    const title = info.data.Title;
+
+    const nominee = {
+      id: id,
+      year: year,
+      title: title,
+    };
+
+    function Duplicate(nominees) {
+      for (let i = 0; i < nominees.length; i++) {
+        if (nominees[i]["id"] === nominee["id"]) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    if (nominees.length < 5) {
+      if (Duplicate(nominees)) {
+        console.log(Duplicate(nominees));
+        console.log("duplicate!!");
+      } else {
+        setNominees([...nominees, nominee]);
+      }
+    } else {
+      setBanner(true);
+    }
+  };
+
   return (
     <div>
-      <SearchBar />
+      {banner && <Banner banner={banner} setBanner={setBanner} />}
+      <SearchBar
+        focusInput={focusInput}
+        text={text}
+        onChange={onChange}
+        fetchMovies={fetchMovies}
+      />
+      <div>
+        {find ? (
+          <ResultFor
+            api={API_KEY}
+            movies={movies}
+            onNomination={onNomination}
+          />
+        ) : (
+          <h3>We can not find any movie with the offered information</h3>
+        )}
+        <Nominations nominees={nominees} setNominees={setNominees} />
+      </div>
     </div>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,89 +1,8 @@
-import React, { useState, useRef } from "react";
-import axios from "axios";
-import ResultFor from "./ResultFor";
 import "./App.css";
-import Banner from "./Banner";
-import Nominations from "./Nominations";
 
-function SearchBar() {
-  const [inputs, setInputs] = useState({
-    text: "",
-    movies: [],
-    find: true,
-  });
-  const [nominees, setNominees] = useState([]);
-  const [banner, setBanner] = useState(false);
-  const { text, movies, find } = inputs;
-  const focusInput = useRef();
-
-  const onChange = (e) => {
-    setInputs({
-      ...inputs,
-      text: e.target.value,
-    });
-  };
-
-  const API_KEY = "https://www.omdbapi.com/?apikey=9cbee72";
-
-  const fetchMovies = async () => {
-    const {
-      data: { Search },
-    } = await axios.get(`${API_KEY}&&s=${text}`);
-    if (Search === undefined) {
-      setInputs({
-        ...inputs,
-        find: false,
-      });
-    } else {
-      setInputs({
-        ...inputs,
-        movies: Search,
-        find: true,
-      });
-    }
-    focusInput.current.focus();
-  };
-
-  const onNomination = async (e) => {
-    const imdbID = e.target.parentElement.firstChild.innerText;
-    const API = `${API_KEY}&i=${imdbID}`;
-    const info = await axios.get(API);
-
-    const id = info.data.imdbID;
-    const year = info.data.Year;
-    const title = info.data.Title;
-
-    const nominee = {
-      id: id,
-      year: year,
-      title: title,
-    };
-
-    function Duplicate(nominees) {
-      for (let i = 0; i < nominees.length; i++) {
-        if (nominees[i]["id"] === nominee["id"]) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    if (nominees.length < 5) {
-      if (Duplicate(nominees)) {
-        console.log(Duplicate(nominees));
-        console.log("duplicate!!");
-      } else {
-        setNominees([...nominees, nominee]);
-      }
-    } else {
-      setBanner(true);
-    }
-  };
-
+function SearchBar({ text, focusInput, onChange, fetchMovies }) {
   return (
     <div>
-      {banner && <Banner banner={banner} setBanner={setBanner} />}
-
       <h1>Movie_app_02</h1>
       <div>
         <h3>Movie title</h3>
@@ -95,19 +14,6 @@ function SearchBar() {
           ref={focusInput}
         />
         <button onClick={fetchMovies}>submit</button>
-      </div>
-      <div>
-        {find ? (
-          <ResultFor
-            api={API_KEY}
-            movies={movies}
-            onNomination={onNomination}
-          />
-        ) : (
-          <h3>We can not find any movie with the offered information</h3>
-        )}
-
-        <Nominations nominees={nominees} setNominees={setNominees} />
       </div>
     </div>
   );
